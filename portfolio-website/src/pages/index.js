23b32f3794d475ed527137eb18b1ec33448f104d
Nuno Ferreira <nuno.ferreira@corepower.energy>
2024-11-09 21:15:07 +0000

    Modified the formatting of files

--- portfolio-website/src/pages/index.js
+++ portfolio-website/src/pages/index.js
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1>Welcome to my Portfolio Website</h1>
+            <p>This is my portfolio website where I will showcase projects that I'm currently working on and past university and personal projects.</p>
         </div>
     );
 };
