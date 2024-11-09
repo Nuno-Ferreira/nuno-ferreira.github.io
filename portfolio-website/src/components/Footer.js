a6aa910dbbf5592b21de4b9544369391f4c070e9
Nuno Ferreira <nuno.ferreira@corepower.energy>
2024-11-09 21:22:57 +0000

    Modified links to open on another tab

--- portfolio-website/src/components/Footer.js
+++ portfolio-website/src/components/Footer.js
@@ -7,14 +7,14 @@ const Footer = () => {
         <>
             <StyledFooter>
                 <FooterMenu>
-                    <FooterLink to="https://reactjs.org" activeStyle>
+                    <FooterLink to="https://reactjs.org" target="_blank" activeStyle>
                         <FooterLogo src={logo} />
                         Created with React.js
                     </FooterLink>
-                    <FooterLink to="https://www.linkedin.com/in/nuno-ferreira-2950741b8/" activeStyle>
+                    <FooterLink to="https://www.linkedin.com/in/nuno-ferreira-2950741b8/" target="_blank" activeStyle>
                         LinkedIn
                     </FooterLink>
-                    <FooterLink to="https://github.com/Nuno-Ferreira" activeStyle>
+                    <FooterLink to="https://github.com/Nuno-Ferreira" target="_blank" activeStyle>
                         GitHub
                     </FooterLink>
                 </FooterMenu>
